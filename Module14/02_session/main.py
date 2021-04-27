ac2ac22ac166abfd95d53d91ce9f401666437af5
fanto <fantom13579000@i.ua>
2021-04-27 21:04:12 +0300

    Задача 2. Сессия

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+x_diff = x2 - x1
+y_diff = y2 - y1
+if x_diff == 0 or y_diff == 0:
+    print('Уравнение равно 0')
+else:
+    k = y_diff / x_diff
+    b = y2 - k * x2
+    print("Уравнение прямой, проходящей через эти точки:")
+    print("y = ", k, " * x + ", b)
+
