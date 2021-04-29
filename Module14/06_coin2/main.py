f74ac0d9119860e4250dcd2313154575d3248aac
fanto <fantom13579000@i.ua>
2021-04-29 20:36:09 +0300

    Задача 6. Монетка 2

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+print('Введите координаты монетки: ')
+x = float(input('X: '))
+y = float(input('Y: '))
+radius = int(input('Введите радиус: '))
+
+if x > radius and y > radius:
+    print('Монетки нет в области')
+else:
+    print('Монетка где-то рядом')
