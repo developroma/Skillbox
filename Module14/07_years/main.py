71c3a252a1cc9d9235576f037a1a6706c5b75267
fanto <fantom13579000@i.ua>
2021-04-29 21:40:35 +0300

    Задача 7. Года

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+first_year = int(input('Введите первый год: '))
+second_year = int(input('Введите второй год: '))
+
+print('Года от ', first_year, 'до ', second_year, 'с тремя одинаковыми числами:')
+num = 0
+num_2 = 0
+num_3 = 0
+num_4 = 0
+while second_year >= first_year:
+    first_year += 1
+    num = first_year % 10
+    num_2 = first_year % 100 // 10
+    num_3 = first_year % 1000 // 100
+    num_4 = first_year % 10000 // 1000
+    if num == num_2 and num == num_4 and num_2 == num_4:
+        print(first_year)
+    elif num_3 == num and num_2 == num_3:
+        print(first_year)
