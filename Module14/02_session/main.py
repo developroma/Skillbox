434b5e0a92e6d9af3a7bed498bad47ea78e49ed3
fanto <fantom13579000@i.ua>
2021-05-01 19:01:29 +0300

    Задача 7. Года

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -7,11 +7,10 @@ y2 = float(input('Y: '))
 
 x_diff = x2 - x1
 y_diff = y2 - y1
-if x_diff == 0 or y_diff == 0:
-    print('Уравнение равно 0')
-else:
+if x_diff > 0 or y_diff > 0:
     k = y_diff / x_diff
     b = y2 - k * x2
     print("Уравнение прямой, проходящей через эти точки:")
     print("y = ", k, " * x + ", b)
-
+elif x_diff == 0 or y_diff == 0:
+    print('X', x_diff, 'Y', ,y_diff)
--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,9 +1,13 @@
+import math
+
 print('Введите координаты монетки: ')
 x = float(input('X: '))
 y = float(input('Y: '))
 radius = int(input('Введите радиус: '))
 
-if x > radius and y > radius:
-    print('Монетки нет в области')
+pythagorean_theorem = math.sqrt(x ** 2 + y ** 2)
+
+if pythagorean_theorem > radius:
+    print('Монетки в области нет')
 else:
     print('Монетка где-то рядом')
