02793435575f8a49120d336dca37f72b90674ce2
fanto <fantom13579000@i.ua>
2021-05-02 19:15:40 +0300

    Задача 3. Клетки

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+count_cells = int(input('Кол-во клеток: '))
+cells = []
+c = []
+
+for i in range(1, count_cells + 1):
+    print('Эффективность', i, 'клетки: ', end='')
+    efficiency = int(input())
+    cells.append(efficiency)
+    if i > cells[i - 1]:
+        c.append(cells[i - 1])
+print('Неподходящие значения:', c)
