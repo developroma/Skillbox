c5e02cd873f3f8c0cde84fd55ee3df80ac07804d
fanto <fantom13579000@i.ua>
2021-05-02 20:15:17 +0300

    Задача 5. Кино

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,17 @@
-# TODO здесь писать код
+count_video = int(input('Кол-во видеокарт: '))
 
+old_list = []
+new_list = []
+
+new = 0
+for i in range(1, count_video + 1):
+    print(i, 'Видеокарта: ', end='')
+    model_video = int(input())
+    old_list.append(model_video)
+    new = model_video
+    if new < model_video:
+        new = model_video
+        new_list.append(new)
+print('Старый список видеокарт:', old_list)
+print('Новый список видеокарт:', new_list)
 
