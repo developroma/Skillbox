8e38b36106c82e722bc3651843977a044ac743f2
fanto <fantom13579000@i.ua>
2021-05-04 23:40:28 +0300

    Задача 8. Бегущие цифры

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+count_containers = int(input('Кол-во контейнеров: '))
+containers = []
+
+for count in range(count_containers):
+    weight_containers = int(input('Введите вес контейнера: '))
+    containers.append(weight_containers)
+
+weight_new_containers = int(input('Введите вес нового контейнера: '))
+
+s = 0
+for i in containers:
+    s += 1
+
+print('Номер, контейнера куда встанет новый контейнер:', s)
--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+shift = int(input('Сдвиг: '))
+old_list = list(input('Изначальный список: '))
+
+print('Изначальный список:', old_list)
+
+old_list.insert(0, old_list[-1 * shift])
+old_list.pop(-1 * shift)
+print('Сдвинутый список:', old_list)
