c162251d19aad4608edd18ee2bc02524af4c458a
fanto <fantom13579000@i.ua>
2021-05-04 19:45:28 +0300

    Задача 4. Видеокарты

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -3,15 +3,14 @@ count_video = int(input('Кол-во видеокарт: '))
 old_list = []
 new_list = []
 
-new = 0
-for i in range(1, count_video + 1):
+for i in range(count_video):
     print(i, 'Видеокарта: ', end='')
     model_video = int(input())
     old_list.append(model_video)
-    new = model_video
-    if new < model_video:
-        new = model_video
-        new_list.append(new)
+for i in old_list:
+    max_model_video = max(old_list)
+    if max_model_video != i:
+        new_list.append(i)
 print('Старый список видеокарт:', old_list)
 print('Новый список видеокарт:', new_list)
 
--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,3 +1,10 @@
 word = input('Введите слово: ')
 letters = list(word)
 
+count = 0
+с = 0
+for i in letters:
+    for i_2 in letters:
+        if i_2 == i:
+            count += 1
+            print(count)
