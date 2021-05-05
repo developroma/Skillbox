d6ccab9c6f0a935dedb49ebb6d5768489a02ee7b
fanto <fantom13579000@i.ua>
2021-05-05 18:42:26 +0300

    Задача 9. Анализ слова 2

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+word = list(word)
+word_palindrome = []
+
+len_word = len(word)
+for i in range(1, len_word + 1):
+    reverse_word = word[-1 * i]
+    word_palindrome.append(reverse_word)
+if word == word_palindrome:
+    print('Слово является палиндромом')
+else:
+    print('Слово не является палиндромом')
