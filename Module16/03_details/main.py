a575207eaa7ac0ca529cd6dd1f84249f424a44e5
fanto <fantom13579000@i.ua>
2021-05-10 22:48:03 +0300

    Задача 3. Детали

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -1,5 +1,15 @@
+name_details = input('Название детали - ')
+
 shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+count_details = 0
+sum_details = 0
+for i in range(len(shop)):
+    if shop[i][0] == name_details:
+        count_details += 1
+        sum_details += shop[i][1]
+print('\nКол-во деталей - ', count_details)
+
+print('Общая стоимость - ', sum_details)
