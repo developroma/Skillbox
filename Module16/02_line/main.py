782917b9da607fba79ebee75736dfde5ae83c9d2
fanto <fantom13579000@i.ua>
2021-05-10 22:09:10 +0300

    Задача 2. Шеренга

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+first_class = list(range(160, 177, 2))
+second_class = list(range(162, 181, 3))
+
+first_class.extend(second_class)
+
+for i in range(len(first_class)):
+    for i_min in range(i, len(first_class)):
+        if first_class[i] > first_class[i_min]:
+            first_class[i_min], first_class[i] = first_class[i], first_class[i_min]
+
+print('Отсортированный список:', first_class)
