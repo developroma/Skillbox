4f59b732c8ff13c5fb8f4f8f6c7fa54bd89c3bea
fanto <fantom13579000@i.ua>
2021-05-25 17:05:22 +0300

    Задача 7. Двумерный список

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+count_stick = int(input('Кол-во палок: '))
+count_shot = int(input('Кол-во бросков: '))
+for shots in range(count_shot):
+    print('Бросок:', shots + 1, end='')
+    left_stick = int(input('Сбиты палки с номера', 'по номер 10'))
--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,9 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+two_d_list = sum(nice_list, [])
+result = []
+for i in two_d_list:
+    for three_d_list in i:
+        result.append(three_d_list)
+print(result)
