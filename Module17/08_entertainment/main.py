ad47f18c76c22b38771b909b519065f97af8f8a0
fanto <fantom13579000@i.ua>
2021-05-27 17:37:37 +0300

    Задача 8. Развлечение

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,5 +1,12 @@
 count_stick = int(input('Кол-во палок: '))
 count_shot = int(input('Кол-во бросков: '))
+
+list_stick = ['|' for _ in range(count_stick)]
 for shots in range(count_shot):
-    print('Бросок:', shots + 1, end='')
-    left_stick = int(input('Сбиты палки с номера', 'по номер 10'))
+    left = int(input('Сбиты палки с номера '))
+    right = int(input('по номер '))
+    print('Бросок', shots + 1, '. Сбиты палки с номера', left, 'по номер', right)
+    new_list = ['.' for _ in range(right - left)]
+    list_stick[left:right] = new_list
+
+print('Результат:', *list_stick)
