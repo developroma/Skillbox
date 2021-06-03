9d7a35f42d7c1ab4a370cf101a6ef50971cd65f7
fanto <fantom13579000@i.ua>
2021-06-03 19:18:19 +0300

    Задача 6. Сжатие

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+count = 1
+letters = ''
+for i in range(len(string)):
+    if string[i] == string[i - 1]:
+        count += 1
+    else:
+        letters += string[i - 1] + str(count)
+        count = 1
+print('Закодированная строка:', letters[2:] + letters[:2])
