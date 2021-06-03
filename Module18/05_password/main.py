65bd101afd00a183b41bd093ec048d9fadf49e6e
fanto <fantom13579000@i.ua>
2021-06-03 17:43:22 +0300

    Задача 5. Пароль

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+count = 0
+
+while True:
+    password = input('Придумайте пароль: ')
+    for i_2 in password:
+        if i_2.isdigit():
+            count += 1
+
+    for i in password:
+        if len(password) >= 8 and i.isupper() and count >= 3:
+            print('Это надёжный пароль!')
+            break
+        else:
+            print('Пароль ненадёжный. Попробуйте ещё раз.')
+            break
