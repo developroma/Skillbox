11aa3c7759833bf1008d604561a2b18f92a84649
fanto <fantom13579000@i.ua>
2021-06-06 20:10:23 +0300

    Задача 8. Бегущая строка

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+fst_string = input('Первая строка: ')
+second_string = input('Вторая строка: ')
+
+clone_fst_string = fst_string[:]
+clone_fst_string *= 2
+
+if second_string in clone_fst_string:
+    shift = clone_fst_string.index(second_string)
+    print('Первая строка получается из второй со сдвигом {shift}'.format(shift=shift))
+else:
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+text = 'Это задание очень! простое'
+sym = '!'
+for i in sym:
+    for x in text:
+        if i != x:
+            pass
+        x += i
+        print(x)
