7683053915d95c8c61e8c03de0eb146f8533dd44
fanto <fantom13579000@i.ua>
2021-06-08 19:59:27 +0300

    Задача 9. Сообщение

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,8 +1,22 @@
-text = 'Это задание очень! простое'
-sym = '!'
-for i in sym:
-    for x in text:
-        if i != x:
-            pass
-        x += i
-        print(x)
+# Это задание очень! простое.
+list_text = list(input('Сообщение: '))
+sym = '!.'
+add_list = []
+s = []
+
+
+for i_text in list_text:
+    for i_sym in sym:
+        if i_sym in i_text:
+            index_sym = list_text.index(i_sym)
+            delete_sym = list_text.pop(index_sym)
+            reverse = list_text[::-1]
+            reverse_join = ''.join(reverse)
+            new_reverse = reverse_join.split()
+            for i_new in range(len(new_reverse)):
+                add_list.insert(i_new, new_reverse[-1 * (i_new + 1)])
+                new_reverse_2 = ' '.join(add_list)
+            for i in new_reverse_2:
+                s.append(i)
+            s.insert(index_sym, delete_sym)
+print(''.join(s))
