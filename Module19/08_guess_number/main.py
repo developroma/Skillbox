5fc34764867f262ec726fa34a9401348a0754205
fanto <fantom13579000@i.ua>
2021-06-23 15:42:28 +0300

    Задача 8. Угадай число

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+max_num = int(input('Введите максимальное число: '))
+need_nums_set_add = set()
+all_answer = ''
+
+while True:
+    need_nums = input('Нужное число есть среди вот этих чисел: ').split()
+    answer = input('Ответ Артёма: ')
+    need_nums_set = set(need_nums)
+    if answer == 'Помогите!':
+        break
+    elif answer == 'Да':
+        for i in need_nums:
+            need_nums_set_add.add(i)
+    elif answer == 'Нет!':
+        intersection = need_nums_set_add.intersection(need_nums_set)
+        all_answer = need_nums_set_add - intersection
+print('Артём мог загадать эти числа:', all_answer)
