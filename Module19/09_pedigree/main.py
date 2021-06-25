7e794db4450e29b577440876148a8bdcdffae5c4
fanto <fantom13579000@i.ua>
2021-06-25 14:51:42 +0300

    Задача 9. Родословная

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+count_peoples = int(input('Введите количество человек: '))
+main_dict = dict()
+new_dict = dict()
+
+for i in range(1, count_peoples):
+    kid, parent = input(f'{i} пара: ').split()
+    main_dict[parent] = kid
+    new_dict[kid] = 0
+    new_dict[parent] = 0
+    if parent in main_dict:
+        new_dict[kid] += 1
+    elif kid in main_dict:
+        new_dict[kid] += 1
+        new_dict[parent] += 1
+
+print('"Высота" каждого члена семьи')
+for i in new_dict:
+    print(i, new_dict[i])
